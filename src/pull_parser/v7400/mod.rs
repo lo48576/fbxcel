6bc126cbdabe63ca52267a2b57a07f74a12da1f2
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-11-23 20:11:46 +0900

    Add trait for types loadable from a reader or a parser

--- src/pull_parser/v7400/mod.rs
+++ src/pull_parser/v7400/mod.rs
@@ -8,7 +8,9 @@ use super::{ParserSource, ParserSourceExt, ParserVersion, Result};
 pub use self::attribute::{Attributes, VisitAttribute};
 pub use self::event::{Event, StartNode};
 pub use self::parser::{from_reader, from_seekable_reader, Parser};
+pub(crate) use self::read::{FromParser, FromReader};
 
 pub mod attribute;
 mod event;
 mod parser;
+mod read;
