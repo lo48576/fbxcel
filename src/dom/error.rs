2536279b595df94c5882281866470c33d7b4845a
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-01-08 15:18:57 +0900

    Move `dom::error::LoadError` to sub `load` module

--- src/dom/error.rs
+++ src/dom/error.rs
@@ -1,41 +1,5 @@
 //! DOM error.
 
-use std::error;
-use std::fmt;
+pub use self::load::LoadError;
 
-use crate::pull_parser::Error as ParserError;
-
-/// Error on DOM load.
-#[derive(Debug)]
-pub enum LoadError {
-    /// Bad parser.
-    ///
-    /// This error will be mainly caused by user logic error.
-    BadParser,
-    /// Parser error.
-    Parser(ParserError),
-}
-
-impl fmt::Display for LoadError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            LoadError::BadParser => f.write_str("Bad parser is given"),
-            LoadError::Parser(e) => write!(f, "Parser error: {}", e),
-        }
-    }
-}
-
-impl error::Error for LoadError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            LoadError::Parser(e) => Some(e),
-            _ => None,
-        }
-    }
-}
-
-impl From<ParserError> for LoadError {
-    fn from(e: ParserError) -> Self {
-        LoadError::Parser(e)
-    }
-}
+mod load;
