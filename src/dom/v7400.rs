da7122ede86703462c7b71d8db3983fc9e63c6fa
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-01-19 02:38:52 +0900

    Use more efficient internal representation for `StrSym`

--- src/dom/v7400.rs
+++ src/dom/v7400.rs
@@ -1,5 +1,7 @@
 //! FBX DOM for FBX 7.4 (or compatible versions).
 
+use string_interner::Sym;
+
 pub use self::core::Core;
 pub use self::document::{Document, Loader};
 pub use self::node::{DowncastId, NodeId};
@@ -13,15 +15,22 @@ pub mod object;
 mod parsed;
 
 /// Symbol for interned string.
+// This is an opaque-typedef pattern.
+// `string_interner::Sym` has efficient implementation, so use it internally.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub(crate) struct StrSym(usize);
+pub(crate) struct StrSym(Sym);
 
 impl string_interner::Symbol for StrSym {
+    /// This may panic if the given value is too large.
+    ///
+    /// As of writing this, string-interner 0.7.0 panics if the given value is
+    /// greater than `u32::max_value() - 1`.
+    /// See [`string_interner::Sym`] for detail.
     fn from_usize(v: usize) -> Self {
-        StrSym(v)
+        StrSym(Sym::from_usize(v))
     }
 
     fn to_usize(self) -> usize {
-        self.0
+        self.0.to_usize()
     }
 }
