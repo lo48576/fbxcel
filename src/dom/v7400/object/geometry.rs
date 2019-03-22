3c6af66050dbfab28f309c1cc9cc8c46ccc0b198
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-03-22 16:43:10 +0900

    Add `geometry::ShapeHandle`

--- src/dom/v7400/object/geometry.rs
+++ src/dom/v7400/object/geometry.rs
@@ -2,15 +2,18 @@
 
 use crate::dom::v7400::object::ObjectHandle;
 
-pub use self::mesh::MeshHandle;
+pub use self::{mesh::MeshHandle, shape::ShapeHandle};
 
 mod mesh;
+mod shape;
 
 define_typed_handle! {
     /// Typed geometry handle.
     TypedGeometryHandle(GeometryHandle) {
         /// Mesh.
         ("Geometry", "Mesh") => Mesh(MeshHandle),
+        /// Shape.
+        ("Geometry", "Shape") => Shape(ShapeHandle),
     }
 }
 
