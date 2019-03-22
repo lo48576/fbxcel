b131a4f1be4ac525ad000cf9d3f875785060e3db
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-03-22 21:23:41 +0900

    Support relations between `geometry::MeshHandle` and `model::MeshHandle`

--- src/dom/v7400/object/geometry/mesh.rs
+++ src/dom/v7400/object/geometry/mesh.rs
@@ -1,8 +1,21 @@
 //! `Geometry` object (mesh).
 
-use crate::dom::v7400::object::geometry::GeometryHandle;
+use crate::dom::v7400::object::{geometry::GeometryHandle, model, TypedObjectHandle};
 
 define_object_subtype! {
     /// `Geometry` node handle (mesh).
     MeshHandle: GeometryHandle
+}
+
+impl<'a> MeshHandle<'a> {
+    /// Returns an iterator of parent model objects.
+    pub fn models(&self) -> impl Iterator<Item = model::MeshHandle<'a>> + 'a {
+        self.source_objects()
+            .filter(|obj| obj.label().is_none())
+            .filter_map(|obj| obj.object_handle())
+            .filter_map(|obj| match obj.get_typed() {
+                TypedObjectHandle::Model(model::TypedModelHandle::Mesh(o)) => Some(o),
+                _ => None,
+            })
+    }
 }
--- src/dom/v7400/object/model/mesh.rs
+++ src/dom/v7400/object/model/mesh.rs
@@ -1,8 +1,30 @@
 //! `Model` object (mesh).
 
-use crate::dom::v7400::object::model::ModelHandle;
+use failure::{format_err, Error};
+
+use crate::dom::v7400::object::{geometry, model::ModelHandle, TypedObjectHandle};
 
 define_object_subtype! {
     /// `Model` node handle (mesh).
     MeshHandle: ModelHandle
+}
+
+impl<'a> MeshHandle<'a> {
+    /// Returns object handle of child geometry object.
+    pub fn geometry(&self) -> Result<geometry::MeshHandle<'a>, Error> {
+        self.destination_objects()
+            .filter(|obj| obj.label().is_none())
+            .filter_map(|obj| obj.object_handle())
+            .filter_map(|obj| match obj.get_typed() {
+                TypedObjectHandle::Geometry(geometry::TypedGeometryHandle::Mesh(o)) => Some(o),
+                _ => None,
+            })
+            .next()
+            .ok_or_else(|| {
+                format_err!(
+                    "Model mesh should have a child geometry mesh, but not found: object={:?}",
+                    self
+                )
+            })
+    }
 }
