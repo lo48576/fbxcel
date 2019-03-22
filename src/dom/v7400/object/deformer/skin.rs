8f8e06766a0550b023ffc6ee908b061c557ead63
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-03-22 23:43:13 +0900

    Support relations between `geometry::MeshHandle` and `deformer::SkinHandle`

--- src/dom/v7400/object/deformer/skin.rs
+++ src/dom/v7400/object/deformer/skin.rs
@@ -1,8 +1,30 @@
 //! `Deformer` object (skin).
 
-use crate::dom::v7400::object::deformer::DeformerHandle;
+use failure::{format_err, Error};
+
+use crate::dom::v7400::object::{deformer::DeformerHandle, geometry, TypedObjectHandle};
 
 define_object_subtype! {
     /// `Deformer` node handle (skin).
     SkinHandle: DeformerHandle
+}
+
+impl<'a> SkinHandle<'a> {
+    /// Returns the parant geometry mesh.
+    pub fn mesh(&self) -> Result<geometry::MeshHandle<'a>, Error> {
+        self.source_objects()
+            .filter(|obj| obj.label().is_none())
+            .filter_map(|obj| obj.object_handle())
+            .filter_map(|obj| match obj.get_typed() {
+                TypedObjectHandle::Geometry(geometry::TypedGeometryHandle::Mesh(o)) => Some(o),
+                _ => None,
+            })
+            .next()
+            .ok_or_else(|| {
+                format_err!(
+                    "Deformer skin object should have a parent geometry mesh: object={:?}",
+                    self
+                )
+            })
+    }
 }
--- src/dom/v7400/object/geometry/mesh.rs
+++ src/dom/v7400/object/geometry/mesh.rs
@@ -1,6 +1,6 @@
 //! `Geometry` object (mesh).
 
-use crate::dom::v7400::object::{geometry::GeometryHandle, model, TypedObjectHandle};
+use crate::dom::v7400::object::{deformer, geometry::GeometryHandle, model, TypedObjectHandle};
 
 define_object_subtype! {
     /// `Geometry` node handle (mesh).
@@ -15,6 +15,17 @@ impl<'a> MeshHandle<'a> {
             .filter_map(|obj| obj.object_handle())
             .filter_map(|obj| match obj.get_typed() {
                 TypedObjectHandle::Model(model::TypedModelHandle::Mesh(o)) => Some(o),
+                _ => None,
+            })
+    }
+
+    /// Returns a child deformer skin if available.
+    pub fn skins(&self) -> impl Iterator<Item = deformer::SkinHandle<'a>> {
+        self.destination_objects()
+            .filter(|obj| obj.label().is_none())
+            .filter_map(|obj| obj.object_handle())
+            .filter_map(|obj| match obj.get_typed() {
+                TypedObjectHandle::Deformer(deformer::TypedDeformerHandle::Skin(o)) => Some(o),
                 _ => None,
             })
     }
