d2bf1f4eef76dfe4d2fdb47d67b76b74b18f9798
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-03-23 23:05:28 +0900

    Make docs under `pull_parser` module more detailed

--- src/pull_parser/v7400.rs
+++ src/pull_parser/v7400.rs
@@ -1,4 +1,8 @@
 //! Parser-related stuff for FBX 7.4 or later.
+//!
+//! To see how to setup a parser, see module documentation of [`pull_parser`].
+//!
+//! [`pull_parser`]: ../index.html
 
 pub(crate) use self::read::{FromParser, FromReader};
 pub use self::{
