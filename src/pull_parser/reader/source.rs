c479b1ccc98317cdd0d1c5567ff917f5690663d2
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-03-23 22:40:43 +0900

    Make docs under `pull_parser::reader` module more detailed

--- src/pull_parser/reader/source.rs
+++ src/pull_parser/reader/source.rs
@@ -7,6 +7,14 @@ use crate::pull_parser::{reader::PositionCacheReader, ParserSource};
 /// Source with plain reader backend.
 ///
 /// This may be inefficient, but works with any reader types.
+/// It is recommended to use [`SeekableSource`] if the reader implements
+/// `std::io::Seek`.
+///
+/// This internally uses `PositionCacheReader`, so users don't need to wrap
+/// readers by `PositionCacheReader` manually.
+///
+/// [`PositionCacheReader`]: struct.PositionCacheReader.html
+/// [`SeekableSource`]: struct.SeekableSource.html
 #[derive(Debug, Clone, Copy)]
 pub struct PlainSource<R> {
     /// Inner reader.
@@ -22,6 +30,23 @@ impl<R: io::Read> PlainSource<R> {
     }
 
     /// Creates a new `PlainSource` with the given offset.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fbxcel::pull_parser::reader::PlainSource;
+    /// use fbxcel::pull_parser::ParserSource;
+    ///
+    /// let msg = "Hello, world!";
+    /// let len = msg.len() as u64;
+    /// let mut reader = std::io::Cursor::new(msg);
+    /// let mut reader = PlainSource::with_offset(&mut reader, 42);
+    ///
+    /// assert_eq!(reader.position(), 42, "Start position is 42");
+    /// std::io::copy(&mut reader, &mut std::io::sink())
+    ///     .expect("Should never fail");
+    /// assert_eq!(reader.position(), len + 42);
+    /// ```
     pub fn with_offset(inner: R, offset: usize) -> Self {
         Self {
             inner: PositionCacheReader::with_offset(inner, offset),
@@ -62,8 +87,14 @@ impl<R: io::Read> ParserSource for PlainSource<R> {
 
 /// Source with seekable reader backend.
 ///
-/// This may be efficient, but works only with reader types implementing
-/// [`std::io::Seek`].
+/// This may be more efficient than [`PlainSource`], but works only with reader
+/// types implementing `std::io::Seek`.
+///
+/// This internally uses [`PositionCacheReader`], so users don't need to wrap
+/// readers by [`PositionCacheReader`] manually.
+///
+/// [`PlainSource`]: struct.PlainSource.html
+/// [`PositionCacheReader`]: struct.PositionCacheReader.html
 #[derive(Debug, Clone, Copy)]
 pub struct SeekableSource<R> {
     /// Inner reader.
@@ -79,6 +110,23 @@ impl<R: io::Read + io::Seek> SeekableSource<R> {
     }
 
     /// Creates a new `SeekableSource` with the given offset.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fbxcel::pull_parser::reader::SeekableSource;
+    /// use fbxcel::pull_parser::ParserSource;
+    ///
+    /// let msg = "Hello, world!";
+    /// let len = msg.len() as u64;
+    /// let mut reader = std::io::Cursor::new(msg);
+    /// let mut reader = SeekableSource::with_offset(&mut reader, 42);
+    ///
+    /// assert_eq!(reader.position(), 42, "Start position is 42");
+    /// std::io::copy(&mut reader, &mut std::io::sink())
+    ///     .expect("Should never fail");
+    /// assert_eq!(reader.position(), len + 42);
+    /// ```
     pub fn with_offset(inner: R, offset: usize) -> Self {
         Self {
             inner: PositionCacheReader::with_offset(inner, offset),
