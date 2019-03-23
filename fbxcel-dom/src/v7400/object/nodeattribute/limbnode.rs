e62dc476183bce5019f0f0095e2f03c27b693067
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-03-23 20:06:59 +0900

    Remove `fbxcel-dom` files entirely

--- fbxcel-dom/src/v7400/object/nodeattribute/limbnode.rs
+++
@@ -1,8 +0,0 @@
-//! `NodeAttribute` object (limbnode).
-
-use crate::v7400::object::nodeattribute::NodeAttributeHandle;
-
-define_object_subtype! {
-    /// `NodeAttribute` node handle (limbnode).
-    LimbNodeHandle: NodeAttributeHandle
-}
--- fbxcel-dom/src/v7400/object/video/clip.rs
+++
@@ -1,8 +0,0 @@
-//! `Video` object (clip).
-
-use crate::v7400::object::video::VideoHandle;
-
-define_object_subtype! {
-    /// `Video` node handle (clip).
-    ClipHandle: VideoHandle
-}
